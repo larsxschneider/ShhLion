c5e675f3991b99e4ad898a009f6c647deeddbdb2
Lars Schneider <larsxschneider@gmail.com>
2011-08-21 17:39:49 +0200

    Initial Commit

---
+++ ShhLion/ShhLion.c
@@ -0,0 +1,11 @@
+#include <mach/mach_types.h>
+
+
+kern_return_t ShhLion_start (kmod_info_t * ki, void * d) {
+    return KERN_SUCCESS;
+}
+
+
+kern_return_t ShhLion_stop (kmod_info_t * ki, void * d) {
+    return KERN_SUCCESS;
+}
